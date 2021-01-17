391e26b7f62c3b871ddb9177f10bce8bb12c2be9
Maveth <maveth1@pm.me>
2021-01-17 13:53:28 -0600

    Sessions can now be added. Also, more comments

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
 const redis = require('redis');
+const uuid = require('uuid').v4;
 
 // load environment variables from .env file
 dotenv.config();
@@ -10,12 +11,14 @@ dotenv.config();
 // initialize express
 const app = express();
 app.use(cookieParser());
+app.use(express.json());
 
 // initialize redis
 const dbClient = redis.createClient();
 
-// map session IDs to user information
-app.use(function (req, res) {
+// read cookies from request and return corresponding session
+// should probably be changed to use querystrings or something
+app.get(function (req, res) {
     // get JWT from cookies
     let token = req.cookies['jwt'];
 
@@ -30,5 +33,26 @@ app.use(function (req, res) {
             // return session info retrieved from DB
             res.json(JSON.parse(reply));
         });
+    });
+});
+
+// create a new session
+// needs to be changed to support custom session time-to-live
+app.post(function (req, res) {
+    // create a unique, random session ID
+    let sid = uuid();
+    
+    // save session info (arbitrary JSON) to DB
+    dbClient.set(sid, JSON.stringify(req.body), function (err, reply) {
+        if (err) res.status(400).json(err);
+    });
+
+    // create and sign new JWT containing the session ID
+    jwt.sign({ sid: sid }, process.env.SECRET, function (err, token) {
+        if (err) res.status(400).json(err);
+
+        // reply with JWT
+        // idk if parsing this matters, but better safe than sorry
+        res.json(JSON.parse(token)); 
     });
 });
