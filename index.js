8dc85cf59018e3eafa315908ddf92ee787960887
MavethGH <65792235+MavethGH@users.noreply.github.com>
2021-01-28 16:39:58 -0600

    Added a util function to keep code DRY

--- index.js
+++ index.js
@@ -16,6 +16,13 @@ app.use(express.json());
 // initialize redis
 const dbClient = redis.createClient();
 
+// utility function for replying to failed API requests with the error
+function handleError(err, res) {
+    if (err) {
+        res.status(400).json(err);
+    }
+}
+
 // read cookies from request and return corresponding session
 // should probably be changed to use querystrings or something
 app.get(function (req, res) {
@@ -24,11 +31,11 @@ app.get(function (req, res) {
 
     // decode and verify JWT
     jwt.verify(token, process.env.SECRET, function (err, decoded) {
-        if (err) res.status(400).json(err);
+        handleError(err, res);
 
         // look up 'sid' field of JWT in Redis DB
         dbClient.get(decoded.sid, function (err, reply) {
-            if (err) res.status(400).json(err);
+            handleError(err, res);
 
             // return session info retrieved from DB
             res.json(JSON.parse(reply));
@@ -44,12 +51,12 @@ app.post(function (req, res) {
     
     // save session info (arbitrary JSON) to DB
     dbClient.set(sid, JSON.stringify(req.body), function (err) {
-        if (err) res.status(400).json(err);
+        handleError(err, res);
     });
 
     // create and sign new JWT containing the session ID
     jwt.sign({ sid: sid }, process.env.SECRET, function (err, token) {
-        if (err) res.status(400).json(err);
+        handleError(err, res);
 
         // reply with JWT
         // idk if parsing this matters, but better safe than sorry
